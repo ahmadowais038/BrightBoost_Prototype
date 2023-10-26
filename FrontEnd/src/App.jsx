6f056e33c7842bfdd75147775cfe2d3e2eb56635
Jam773 <89766767+Jam773@users.noreply.github.com>
2023-10-26 22:47:13 +1100

    Add files via upload

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -5,12 +5,22 @@ import StudentDashboard from './pages/StudentDashboard';
 import Admin from './pages/AdminDashboard';
 // import LogReg from "./compenents/LogReg"
 import Login from "./pages/Reg";
-import Home from'./pages/HomePage';
+import Home from './pages/HomePage';
+import AdminDashboard from './pages/AdminDashboard';
 import Tutor from "./pages/TutorDashboard"
+import { Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Home />
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/dashboard" element={<AdminDashboard />} />
+      <Route path = "/tutordashboard" element = {<Tutor/>} />
+    </Routes>
+
+
+
   );
 }
 
