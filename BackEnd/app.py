81acb1ef5db48fae504dc37a151d3b50c11be913
ahmadowais038 <125301334+ahmadowais038@users.noreply.github.com>
2023-10-26 20:12:08 +1100

    git commit

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -19,60 +19,83 @@ SQL_DB = SQLAlchemy(app)
 CORS(app)  # Enable CORS for the entire app
 
 
-class Student(SQL_DB.Model):
-    __tablename__ = 'students'
-    Account_ID = SQL_DB.Column(SQL_DB.Integer, primary_key=True)
-    First_Name = SQL_DB.Column(SQL_DB.String(25))
-    Last_Name = SQL_DB.Column(SQL_DB.String(25))
+class Accounts(SQL_DB.Model):
+    __tablename__ = 'Accounts'
+    __table_args__ = {'extend_existing': True}
+    Account_ID = SQL_DB.Column(SQL_DB.String, primary_key=True)
+    Role = SQL_DB.Column(SQL_DB.String(25))
     Email = SQL_DB.Column(SQL_DB.String(255), nullable=False)
-    Phone_Num = SQL_DB.Column(SQL_DB.String(255))
     Username = SQL_DB.Column(SQL_DB.String(25))
     Password = SQL_DB.Column(SQL_DB.String(25))
-    Card_Num = SQL_DB.Column(SQL_DB.String(19))
-    CVV = SQL_DB.Column(SQL_DB.String(3))
-    Expiry = SQL_DB.Column(SQL_DB.String(5))
+    
+    students = SQL_DB.relationship('Student', backref='account', lazy=True)
+    tutors = SQL_DB.relationship('Tutors', backref='account', lazy=True)
+    
+    def __repr__(self):
+        return f'User("{self.Account_ID}","{self.role}","{self.email}","{self.uname}","{self.password}")'
+
+class Approve(SQL_DB.Model):
+    __tablename__ = 'registration'
+    __table_args__ = {'extend_existing': True}
+    Account_ID = SQL_DB.Column(SQL_DB.String, primary_key=True)
+    First_Name = SQL_DB.Column(SQL_DB.String(25))
+    Last_Name = SQL_DB.Column(SQL_DB.String(25))
+    Role = SQL_DB.Column(SQL_DB.String(25))
+    Email = SQL_DB.Column(SQL_DB.String(255), nullable=False)
+    Phone_Num = SQL_DB.Column(SQL_DB.String(12))
+    Username = SQL_DB.Column(SQL_DB.String(25))
+    Password = SQL_DB.Column(SQL_DB.String(25))
     Subjects = SQL_DB.Column(SQL_DB.String(150))
 
     def __repr__(self):
-        return f'User("{self.Student_ID}","{self.First_Name}","{self.Last_Name}","{self.Email}","{self.Phone_Num}","{self.Username}","{self.Password}","{self.Card_Num}","{self.CVV}","{self.Expiry}","{self.Subjects}")'
+        return f'User("{self.Account_ID}","{self.FName}","{self.LName}","{self.email}","{self.Phone_Num}","{self.uname}","{self.password}","{self.Subjects}")'
+
+
+class Student(SQL_DB.Model):
+    __tablename__ = 'students'
+    __table_args__ = {'extend_existing': True}
+    Account_ID = SQL_DB.Column(SQL_DB.String, SQL_DB.ForeignKey('Accounts.Account_ID'), primary_key=True)
+    First_Name = SQL_DB.Column(SQL_DB.String(25))
+    First_Name = SQL_DB.Column(SQL_DB.String(25))
+    Last_Name = SQL_DB.Column(SQL_DB.String(25))
+    Email = SQL_DB.Column(SQL_DB.String(255), nullable=False)
+    Phone_Num = SQL_DB.Column(SQL_DB.String(12))
+    Subjects = SQL_DB.Column(SQL_DB.String(150))
+    Card_Num = SQL_DB.Column(SQL_DB.String(19))
+    CVV = SQL_DB.Column(SQL_DB.String(3))
+    Expiry = SQL_DB.Column(SQL_DB.String(5))
+
+    def __repr__(self):
+        return f'User("{self.Account_ID}","{self.First_Name}","{self.Last_Name}","{self.Email}","{self.Phone_Num}","{self.Subjects}","{self.Card_Num}","{self.CVV}","{self.Expiry}")'
 
 
 class Tutors(SQL_DB.Model):
     __tablename__ = 'tutors'
-    Account_ID = SQL_DB.Column(SQL_DB.Integer, primary_key=True)
+    __table_args__ = {'extend_existing': True}
+    Account_ID = SQL_DB.Column(SQL_DB.String, SQL_DB.ForeignKey('Accounts.Account_ID'), primary_key=True)
+    First_Name = SQL_DB.Column(SQL_DB.String(25))
     First_Name = SQL_DB.Column(SQL_DB.String(25))
     Last_Name = SQL_DB.Column(SQL_DB.String(25))
     Email = SQL_DB.Column(SQL_DB.String(255), nullable=False)
     Phone_Num = SQL_DB.Column(SQL_DB.String(255))
-    Username = SQL_DB.Column(SQL_DB.String(25))
-    Password = SQL_DB.Column(SQL_DB.String(25))
     Subjects = SQL_DB.Column(SQL_DB.String(150))
 
     def __repr__(self):
-        return f'User("{self.Student_ID}","{self.First_Name}","{self.Last_Name}","{self.Email}","{self.Phone_Num}","{self.Username}","{self.Password}","{self.Subjects}")'
+        return f'User("{self.Account_ID}","{self.First_Name}","{self.Last_Name}","{self.Email}","{self.Phone_Num}","{self.Subjects}")'
 
 
-class Approve(SQL_DB.Model):
-    __tablename__ = 'registration'
-    Account_ID = SQL_DB.Column(SQL_DB.Integer, primary_key=True)
-    FName = SQL_DB.Column(SQL_DB.String(25))
-    LName = SQL_DB.Column(SQL_DB.String(25))
-    email = SQL_DB.Column(SQL_DB.String(255), nullable=False)
-    Phone_Num = SQL_DB.Column(SQL_DB.String(255))
-    uname = SQL_DB.Column(SQL_DB.String(25))
-    password = SQL_DB.Column(SQL_DB.String(25))
-    Subjects = SQL_DB.Column(SQL_DB.String(150))
+# class Accounts(SQL_DB.Model):
+#     __tablename__ = 'accounts'
+#     __table_args__ = {'extend_existing': True}
+#     Account_ID = SQL_DB.Column(SQL_DB.String, primary_key=True)
+#     role = SQL_DB.Column(SQL_DB.String(25))
+#     email = SQL_DB.Column(SQL_DB.String(255), nullable=False)
+#     uname = SQL_DB.Column(SQL_DB.String(25))
+#     password = SQL_DB.Column(SQL_DB.String(25))
 
-    def __repr__(self):
-        return f'User("{self.ID}","{self.FName}","{self.LName}","{self.email}","{self.Phone_Num}","{self.uname}","{self.password}","{self.Subjects}")'
-
-class Accounts(SQL_DB.Model):
-    __tablename__ = 'Accounts'
-    Account_ID = SQL_DB.Column(SQL_DB.String, primary_key=True)
-    email = SQL_DB.Column(SQL_DB.String(255), nullable=False)
-    uname = SQL_DB.Column(SQL_DB.String(25))
-    password = SQL_DB.Column(SQL_DB.String(25))
-
+#     def __repr__(self):
+#         return f'User("{self.Account_ID}","{self.role}","{self.email}","{self.uname}","{self.password}")'
+    
 
 @app.route('/')
 def hello():
@@ -91,81 +114,90 @@ def Register():
     password = data.get('password')
     subjects = data.get('subjects')
 
+    existing_email = Accounts.query.filter_by(Email=Email).first()
+    existing_uname = Accounts.query.filter_by(Username=Username).first()
     
+    if existing_email and existing_uname:
+        return jsonify({"message": "Account associated with this email and username already exists"}), 400
+
+    elif existing_email:
+        return jsonify({"message": "Account associated with this email already exists"}), 400
+
+    elif existing_uname:
+        return jsonify({"message": "Account associated with this username already exists"}), 400
 
     if role == "Student":
-        Card_Num = data.get('cardnum')
-        CVV = data.get('CVV')
-        expiry = data.get('expiry')
         num_existing_students = Student.query.count()
         user_id = f'S15{num_existing_students + 1:02}'
 
-
-        existing_email = Student.query.filter_by(Email=Email).first()
-        existing_uname = Student.query.filter_by(Username=Username).first()
-
-        if existing_email and existing_uname:
-            return jsonify({"message": "Account associated with this email and username already exists"}), 400
-
-        elif existing_email:
-            return jsonify({"message": "Account associated with this email already exists"}), 400
-
-        elif existing_uname:
-            return jsonify({"message": "Account associated with this username already exists"}), 400
-
+        Card_Num = data.get('cardnum')
+        CVV = data.get('CVV')
+        expiry = data.get('expiry')
+        
+        
+        new_Account = Accounts(
+            Account_ID = user_id,
+            Role = role,
+            Email=Email,
+            Username=Username,
+            Password=password
+        )
+        
         new_student = Student(
             Account_ID = user_id,
             First_Name=FName,
             Last_Name=LName,
             Email=Email,
             Phone_Num=phone,
-            Username=Username,
-            Password=password,
             Subjects=subjects,
             Card_Num=Card_Num,
             CVV=CVV,
             Expiry=expiry
         )
-
+        
+        SQL_DB.session.add(new_Account)
         SQL_DB.session.add(new_student)
         SQL_DB.session.commit()
         return jsonify({"message": "Account Successfully registered"}), 200
 
     elif role == "Tutor":
-        existing_email = Tutors.query.filter_by(Email=Email).first()
-        existing_uname = Tutors.query.filter_by(Username=Username).first()
         num_existing_tutors = Tutors.query.count()
         user_id = f'T11{num_existing_tutors + 1:02}'
 
-
-        if existing_email and existing_uname:
-            return jsonify({"message": "Account associated with this email and username already exists"}), 400
-
-        elif existing_email:
-            return jsonify({"message": "Account associated with this email already exists"}), 400
-
-        elif existing_uname:
-            return jsonify({"message": "Account associated with this username already exists"}), 400
+        await_email = Approve.query.filter_by(Email=Email).first()
         
+        if await_email:
+            return jsonify({"message": "Account already registered, awaiting Admin Approval"}), 400
+
         new_Tutor = Approve(
             Account_ID = user_id,
-            FName=FName,
-            LName=LName,
-            email=Email,
+            First_Name=FName,
+            Last_Name=LName,
+            Role=role,
+            Email=Email,
             Phone_Num=phone,
-            uname=Username,
-            password=password,
+            Username=Username,
+            Password=password,
             Subjects=subjects,
         )
 
         SQL_DB.session.add(new_Tutor)
         SQL_DB.session.commit()
-        return jsonify({"message": "Tutor registration pending approval"}), 200
+        return jsonify({"message": "Tutor registration pending Admin approval"}), 200
 
 @app.route('/api/log', methods=['POST'])
 def LogIn():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
 
-    pass
+    is_user = Student.query.filter_by(Username=username, Password=password).first()
+    
+    if is_user:
+        return jsonify({"message": "Login successful"}), 200
 
+    else:
+        return jsonify({"message": "Invalid login credentials"}), 400
+    
 if __name__ == '__main':
     app.run(debug=True)
